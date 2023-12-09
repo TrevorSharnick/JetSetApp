07a92ccb07bf4ba791147ca878f3ee2d1b7800e7
Nate Fuller <nathanernestfuller@gmail.com>
2023-12-09 11:32:28 -0500

    Some view improvements
    * use section headers
    * use navigation titles
    * remove unnecessary VStack from TitleView
    * remove extra NavigationView from MenuView

--- JetSet/JetSet/MenuView.swift
+++ JetSet/JetSet/MenuView.swift
@@ -9,88 +9,85 @@ import SwiftUI
 
 struct MenuView: View {
     var body: some View {
-        
-        NavigationView {
-            ZStack {
-                RadialGradientView()
-                VStack {
-                    // Allows the HStack of Aircraft profiles to scroll side to side
-                    ScrollView(.horizontal, showsIndicators: false) {
-                        HStack(spacing: 20) {
-                            Spacer() // Centers the first profile
-                            // TODO: Redirects the user to their saved jet profile checklist group
-                            Button(action: {
-                                print("Button tapped: Warthog profile")
-                                
-                            }) {
-                                NavigationLink(destination: WarthogDemoTodos()) {
-                                    Rectangle()
-                                        .fill(Color.white)
-                                        .frame(width: 300, height: 300)
-                                        .cornerRadius(20)
-                                        .overlay(
-                                            VStack {
-                                                // Aircraft name
-                                                Text("A-10 Warthog")
-                                                    .font(.system(size: 24, weight: .bold))
-                                                    .foregroundColor(.black)
-                                                    .offset(y: -30)
-                                                
-                                                Image("Warthog")
-                                                    .resizable()
-                                                    .aspectRatio(contentMode: .fill)
-                                                    .frame(width: 150, height: 150)
-                                                    .offset(y: -30)
-                                                
-                                            }
-                                        )
-                                }
+        ZStack {
+            RadialGradientView().ignoresSafeArea()
+            VStack {
+                // Allows the HStack of Aircraft profiles to scroll side to side
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 20) {
+                        Spacer() // Centers the first profile
+                        // TODO: Redirects the user to their saved jet profile checklist group
+                        Button(action: {
+                            print("Button tapped: Warthog profile")
+                            
+                        }) {
+                            NavigationLink(destination: WarthogDemoTodos()) {
+                                Rectangle()
+                                    .fill(Color.white)
+                                    .frame(width: 300, height: 300)
+                                    .cornerRadius(20)
+                                    .overlay(
+                                        VStack {
+                                            // Aircraft name
+                                            Text("A-10 Warthog")
+                                                .font(.system(size: 24, weight: .bold))
+                                                .foregroundColor(.black)
+                                                .offset(y: -30)
+                                            
+                                            Image("Warthog")
+                                                .resizable()
+                                                .aspectRatio(contentMode: .fill)
+                                                .frame(width: 150, height: 150)
+                                                .offset(y: -30)
+                                            
+                                        }
+                                    )
                             }
                         }
                     }
-                    
-                    // Decided to encapsulate the prompt and button necessary for creating new jet profiles in a separate VStack
-                    VStack {
-                        Text("Create a New Jet Profile")
-                            .font(.system(size: 24, weight: .bold))
-                            .padding(.top, 59)
-                            .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5)
-                            
-                        // Button to Create a new Jet profile
-                        Button(action: {
-                            // Action to perform when the button is tapped
-                            print("Button tapped: Create a new jet profile")
-                            // TODO: Add the code and view necessary for the user to create their own jet profile
-                            
-                        }) {
-                            
-                            /*
-                             In SwiftUI, views are layered in a Z-stack from back to front. Since the overlay is on top of the button, it can intercept touch events. To resolve this issue, you can modify your layout by placing the overlay inside the button itself rather than as a separate modifier.
-                             */
-                            ZStack {
-                                Circle()
-                                    .frame(width: 80, height: 80)
-                                    .foregroundColor(.yellow)
-                                    .shadow(color: Color.black.opacity(0.5), radius: 5, x: 0, y: 5)
-                                    
-                                Image(systemName: "person.circle")
-                                    .font(.system(size: 80))
-                                    .foregroundColor(.white)
-                                
-                                    
-                            }
-                        } .padding(.top, 20)
-                       
-                    }
                 }
-                .padding()
+                
+                // Decided to encapsulate the prompt and button necessary for creating new jet profiles in a separate VStack
+                VStack {
+                    Text("Create a New Jet Profile")
+                        .font(.system(size: 24, weight: .bold))
+                        .padding(.top, 59)
+                        .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5)
+                    
+                    // Button to Create a new Jet profile
+                    Button(action: {
+                        // Action to perform when the button is tapped
+                        print("Button tapped: Create a new jet profile")
+                        // TODO: Add the code and view necessary for the user to create their own jet profile
+                        
+                    }) {
+                        
+                        /*
+                         In SwiftUI, views are layered in a Z-stack from back to front. Since the overlay is on top of the button, it can intercept touch events. To resolve this issue, you can modify your layout by placing the overlay inside the button itself rather than as a separate modifier.
+                         */
+                        ZStack {
+                            Circle()
+                                .frame(width: 80, height: 80)
+                                .foregroundColor(.yellow)
+                                .shadow(color: Color.black.opacity(0.5), radius: 5, x: 0, y: 5)
+                            
+                            Image(systemName: "person.circle")
+                                .font(.system(size: 80))
+                                .foregroundColor(.white)
+                            
+                            
+                        }
+                    } .padding(.top, 20)
+                    
+                }
             }
-            
-            .navigationBarTitle("Profiles", displayMode: .inline)
-            
+            .padding()
         }
+        .navigationTitle("Profiles")
     }
 }
 #Preview {
-    MenuView()
+    NavigationStack {
+        MenuView()
+    }
 }
